8e1d5628006bf2d433463f596b1e6a373ac2831e
komangtio049 <127291407+komangtio049@users.noreply.github.com>
2023-07-04 19:03:42 +0800

    main.cpp

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -10,10 +10,16 @@ int main(int argc, char** argv) {
 		list1.insertToHead(2);
 		list1.insertToHead(7);
 		list1.insertToTail(1);
-		list1.insertAfter(18, 1);
-		list1.insertAfter(9, 2);
+		list1.deleteToHead();
 		list1.insertToTail(28);
-		list1.insertAfter(11, 28);
+		list1.insertAfter(11, 1);
+		list1.insertAfter(9, 2);
+		list1.deleteToTail();
+		list1.deleteByValue(9);
+		list1.insertAfter(23, 11);
+		list1.insertAfter(35, 1);
+		list1.insertAfter(89, 1);
+		list1.deleteByValue(1);					
 		
 		
 		cout << "Head:" <<list1.head->value << endl;
