be7858d764b794bc5fa50e05078882cf0341fc47
komangtio049 <127291407+komangtio049@users.noreply.github.com>
2023-07-04 19:02:41 +0800

    LinkedList.cpp

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -59,19 +59,88 @@ void LinkedList::insertAfter(int input1, int input2)
 	
 }
 
+void LinkedList::deleteToHead()
+{
+	if (head==NULL)
+	return;
+	
+	if (head!=NULL)
+	{
+		Node *tmp = head;
+		head = head->next;
+		delete tmp;
+	}
+	
+}
 
+void LinkedList::deleteToTail()
+{
+	if (head==NULL)
+	{
+		return;
+	}
+	
+	if (tail==head)
+	{
+		delete head;
+		head = NULL;
+		tail = NULL;
+		return;
+	}
+	
+	if (tail!=head)
+	{	
+		Node *tmp = head;
+		while (tmp->next != tail)
+		{
+			tmp = tmp->next;
+		}
+		delete tail;
+		tail = tmp;
+		tail->next = NULL;
+	}
+}
+
+void LinkedList::deleteByValue(int input1)
+{
+	Node* tmpdel = NULL;
+	Node* tmp = head;
+
+	while (tmp != NULL && tmp->value != input1)
+	{
+		tmpdel = tmp;
+		tmp = tmp->next;
+ }
+
+	if (tmp != NULL)
+	{
+		if (tmp == head)
+		{
+			head = tmp->next;
+		}
+		else
+		{
+			tmpdel->next = tmp->next;
+		}
+
+		if (tmp == tail)
+		{
+			tail = tmpdel;
+		}
+
+		delete tmp;
+	}
+
+}
 
 void LinkedList::printAll(){
 	
-	
 Node *tmp = head;
 while (tmp != NULL)	{
-	
 	cout << tmp->value << " -> ";
 	
 	tmp = tmp->next;
-	
+
+	}
 }
 	
-	
-}
